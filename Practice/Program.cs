dea09e78f8e26452801f236cef816c250a950b0e
NargizRamazanova <nargiz.ramazanova@code.edu.az>
2022-11-01 19:20:06 +0400

    init

---
+++ Practice/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Practice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Task- 1
+
+            //int[] arr = { 4, 1, 5, 7, 3 };
+            //int[] reversedArr = new int[arr.Length];
+
+            //for(int i=0; i<arr.Length; i++)
+            //{
+            //    reversedArr[(arr.Length - 1) - i] = arr[i];
+            //}
+
+
+
+            //Task-5
+
+            //string[] fruits = { "Banana", "Apple", "Orange" };
+            //string newFruit = "Mango";
+
+            //string[] newFruitArr = new string[fruits.Length+1];
+
+            //newFruitArr[0] = newFruit;
+
+            //for(int i = 0; i<fruits.Length; i++)
+            //{
+            //    newFruitArr[i + 1] = fruits[i];
+            //}
+
+            //for(int i = 0; i<newFruitArr.Length; i++)
+            //{
+            //    Console.Write(newFruitArr[i]+", ");
+            //}
+
+        }
+    }
+}
